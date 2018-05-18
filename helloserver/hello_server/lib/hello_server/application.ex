456a02f3bcf2f53065863841b9564a7fec23e3f7
Lorenzo Grandi <lorenzo@wetransfer.com>
2018-05-18 10:00:32 +0200

    Hello server init

---
+++ helloserver/hello_server/lib/hello_server/application.ex
@@ -0,0 +1,22 @@
+defmodule HelloServer.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: HelloServer.Worker.start_link(arg)
+      # {HelloServer.Worker, arg},
+      Plug.Adapters.Cowboy.child_spec(:http, HelloServer.Plug, [], port: 8080)
+    ]
+
+    IO.puts "Started the application!"
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: HelloServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
